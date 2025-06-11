4dae875780f346efde5cd4dc38a4cacfbaa58314
CinnamonShake45 <shikhardixit09@gmail.com>
2025-06-11 17:01:55 +0530

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,41 @@
 #include <iostream>
-#include "imageread.h"
+#include <vector>
+#include <stdint.h>
+
 #include <png.h>
 
-int main() {
-    std::string filePath;
-    std::cout << "Enter the image file path: ";
-    std::getline(std::cin, filePath);
+#include "imageread.h"
 
-    std::string type = ImageReader::getImageType(filePath);
+int main() {
+    const char* filePath = "C:/Users/Shikhar Dixit/Self Projects/ImageProcex/test-stuff/bigImage.png";
+
+    std::string type = ImageReader::get_image_type(filePath);
     std::cout << "Identified type: " << type << std::endl;
+
+    if(type != "PNG"){
+        std::cout << "Please input only a .png file (for now)" << std::endl;
+        return 0;
+    }
+
+    int width, height;
+
+    std::vector<uint8_t> imagedata;
+
+    try{    
+        if(type == "PNG")
+            imagedata = ImageReader::read_png_file(filePath, width, height, 0);
+    }
+    catch (const char* error){
+        std::cout << error << std::endl;
+    }
+
+    for(int y = 0; y < width; ++y){
+        for(int x = 0; x < height; ++x){
+            const uint8_t* pixel = &imagedata[(y * width + x) * 1];
+            std::cout << (int)pixel[0] << "\t";
+        }
+        std::cout << "\n";
+    }
 
     return 0;
 }
