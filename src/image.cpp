dda005b82337f7fd82906f2822bb27554de0a644
CinnamonShake45 <shikhardixit09@gmail.com>
2026-01-04 15:02:02 +0530

    finalized image class

--- src/image.cpp
+++ src/image.cpp
@@ -1,34 +1,45 @@
 #include "image.h"
+#include <cstddef>
 #include <cstdint>
+#include <stdexcept>
 
-image::image() {
-  imageRows = 0;
-  imageColumns = 0;
-  imageChannels = 0;
-  pixelData = nullptr;
+image::image() : rows(0), columns(0), channels(0) {};
+
+image::image(std::size_t rows, std::size_t columns, std::size_t channels)
+    : rows(rows), columns(columns), channels(channels),
+      data(rows * columns * channels) {
+  if (channels != 1 || channels != 3)
+    throw std::invalid_argument(
+        "image: channels can only be either 1 (grayscale) or 3 (color)");
 }
 
-image::image(std::uint64_t imageRows, std::uint64_t imageColumns,
-             std::uint8_t imageChannels) {
-  this->imageRows = imageRows;
-  this->imageColumns = imageColumns;
-  this->imageChannels = imageChannels;
+image::~image() = default;
+image::image(const image &) = default;
+image::image(image &&) noexcept = default;
+image &image::operator=(const image &) = default;
 
-  pixelData = new std::uint8_t **[imageRows];
-  for (std::size_t i = 0; i < imageRows; i++) {
-    pixelData[i] = new std::uint8_t *[imageColumns];
-    for (std::size_t j = 0; j < imageColumns; j++) {
-      pixelData[i][j] = new std::uint8_t[imageChannels];
-      for (std::uint8_t k = 0; k < imageChannels; k++) {
-        pixelData[i][j][k] = 0;
-      }
-    }
-  }
+std::uint8_t &image::at(std::size_t row, std::size_t col, std::size_t ch) {
+  return data[index(row, col, ch)];
 }
 
-image::~image() {
-  imageRows = 0;
-  imageColumns = 0;
-  imageChannels = 0;
-  delete pixelData;
+const std::uint8_t &image::at(std::size_t row, std::size_t col,
+                              std::size_t ch) const {
+  return data[index(row, col, ch)];
 }
+
+std::uint8_t *image::rawpixels() noexcept { return data.data(); }
+
+const std::uint8_t *image::rawpixels() const noexcept { return data.data(); }
+
+std::size_t image::getRows() const noexcept { return rows; }
+
+std::size_t image::getColumns() const noexcept { return columns; }
+
+std::size_t image::getChannels() const noexcept { return channels; }
+
+std::size_t image::index(std::size_t row, std::size_t col,
+                         std::size_t ch) const {
+  if (row >= rows || col >= columns || ch >= channels)
+    throw std::out_of_range("image: index out of range");
+  return (row * columns + col) * channels + ch;
+};
--- src/image.h
+++ src/image.h
@@ -1,22 +1,37 @@
 #ifndef IMAGE_H
 #define IMAGE_H
 
+#include <cstddef>
 #include <cstdint>
+#include <iterator>
+#include <vector>
 
 class image {
 public:
-  std::uint8_t ***pixelData; // row, columns, channels
-  std::size_t imageRows;
-  std::size_t imageColumns;
-  std::uint8_t imageChannels;
-
   image();
-  image(uint64_t imageRows, uint64_t imageColumns, uint8_t imageChannels);
-  image(image &&) = default;
-  image(const image &) = default;
-  image &operator=(image &&) = default;
-  image &operator=(const image &) = default;
+  image(std::size_t rows, std::size_t columns, std::size_t channels);
+  image(image &&) noexcept;
+  image(const image &);
+  image &operator=(image &&) noexcept;
+  image &operator=(const image &);
   ~image();
+
+  std::uint8_t &at(std::size_t row, std::size_t col, std::size_t ch);
+  const uint8_t &at(std::size_t row, std::size_t c, std::size_t ch) const;
+
+  std::uint8_t *rawpixels() noexcept;
+  const std::uint8_t *rawpixels() const noexcept;
+
+  std::size_t getRows() const noexcept;
+  std::size_t getColumns() const noexcept;
+  std::size_t getChannels() const noexcept;
+
+private:
+  std::vector<std::uint8_t> data; // row, columns, channels format
+  std::size_t rows;
+  std::size_t columns;
+  std::size_t channels;
+  std::size_t index(std::size_t row, std::size_t col, std::size_t ch) const;
 };
 
 #endif /* IMAGE_H */
