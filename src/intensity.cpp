71b5d4c5c5fd0b61f8be4afa275c3c660315519b
cinnamonshake45 <shikhardixit09@gmail.com>
2026-03-09 13:46:09 +0530

    wrote image inversion logic

--- src/intensity.cpp
+++ src/intensity.cpp
@@ -1,6 +1,13 @@
 #include "intensity.h"
 
 Image invert(const Image& img){
-    Image invImg = new Image();
+    Image invImg = new Image(img.getRows, img.getColumns, img.getChannels);
+	for(size_t r = 0; r < img.getRows(); r++)	{
+		for (size_t c = 0; c < img.getColumns(); c++) {
+			for (size_t ch = 0; ch < img.getChannels(); ch++) {
+				invImg.at(r, c, ch) = 255 - img.at(r, c, ch);
+			}
+		}
+	}
     return invImg;
 }
