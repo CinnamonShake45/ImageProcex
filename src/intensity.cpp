77b771730e57029a3271a65a8fce42d8b230cef6
CinnamonShake45 <shikhardixit09@gmail.com>
2026-03-09 12:48:51 +0530

    inversion logic basic

---
+++ src/intensity.cpp
@@ -0,0 +1,6 @@
+#include "intensity.h"
+
+Image invert(const Image& img){
+    Image invImg = new Image();
+    return invImg;
+}
---
+++ src/intensity.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "Image.h"
+
+Image invert(const Image& img);
