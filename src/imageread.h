ab43a51a458f2235bbd911b735a06d567cf40bab
cinnamonshake45 <shikhardixit09@gmail.com>
2026-03-09 19:14:12 +0530

    minor naming fix

--- src/imageread.h
+++ src/imageread.h
@@ -4,11 +4,11 @@
 #include "Image.h"
 #include <cstdint>
 #include <string>
-#include <vector>
+
 class ImageReader {
 public:
   static std::string get_image_type(const char *filePath);
-  static Image read_png(const char *filepath, std::uint8_t mode); // mode : 0 for grayscale, anything else for RGB
+  static Image read_png(const char *filePath, std::uint8_t mode); // mode : 0 for grayscale, anything else for RGB
 };
 
 #endif
