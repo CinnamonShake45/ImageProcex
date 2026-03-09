a65e0afd3677630a5adab60cefd98b49025883ef
cinnamonshake45 <shikhardixit09@gmail.com>
2026-03-09 18:49:09 +0530

    refactored

--- src/imageread.h
+++ src/imageread.h
@@ -8,9 +8,7 @@
 class ImageReader {
 public:
   static std::string get_image_type(const char *filePath);
-  static Image
-  read_png(const char *filepath,
-           std::uint8_t mode); // mode : 0 for grayscale, anything else for RGB
+  static Image read_png(const char *filepath, std::uint8_t mode); // mode : 0 for grayscale, anything else for RGB
 };
 
 #endif
