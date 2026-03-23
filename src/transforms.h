ff6ef674fab1a1f5c97ef9a79b545682c63cb436
cinnamonshake45 <shikhardixit09@gmail.com>
2026-03-23 16:20:13 +0530

    method to apply transform

--- src/transforms.h
+++ src/transforms.h
@@ -1,4 +1,21 @@
+#ifndef TRANSFORMS_H
+#define TRANSFORMS_H
+
 #include "Image.h"
-class transforms {
+#include "intensity.h"
+
+class Transforms {
 public:
+    template<typename Func>
+    static Image apply_transform(const Image& img, Func f) {
+        Image out = img;
+        for (auto& px : out)
+            px = f(px);
+        return out;
+    }
+
+private:
+    Transforms() = delete;
 };
+
+#endif
