016782eaeb633ec918870a3dd4b17b15623e0ea4
cinnamonshake45 <shikhardixit09@gmail.com>
2026-03-29 21:56:07 +0530

    refactored

--- src/exposure.h
+++ src/exposure.h
@@ -2,20 +2,28 @@
 #define EXPOSURE_H
 
 #include "Image.h"
-#include <array>
+#include <vector>
 
-enum class histogramMode{
-    GRAY,
-    RGB
+enum class HistogramMode{
+    GRAYSCALE, /* RGB Image converted to GRAYSCALE then histogram returned (vector of size 1)*/
+    FLATTENED, /* Flatten channels then histogram returned (vector of size 1)*/
+    PER_CHANNEL /* If Image is RGB then per channel histogram returned (vector of size 3) */
+};
+
+template <typename T> /* T: int (counts) or float (normalized) */
+struct Histogram{
+    std::vector<std::vector<T>> data; /* data[channel][bin_index] */
+    std::vector<float> bin_centers;
 };
 
 class Exposure{
-private:
-    std::array<int, 256> histogram;
 public:
-    static std::array<int, 256> get_histogram(const Image& img, histogramMode m);
-    static std::array<float, 256> normalize(const Image& img);
+    static std::vector<float> get_bin_centers(int bin_count, int bin_width);
+    static Histogram<int> get_histogram(Image img, HistogramMode mode, int bin_width); 
+    static Histogram<float> normalize(const Histogram<int>& hist);
     static Image equalize(const Image& img);
+private:
+    Exposure() = delete;
 };
 
 #endif /* EXPOSURE_H */
